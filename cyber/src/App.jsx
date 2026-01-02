8f391fe0d938aa6eb4fe89cf6347c490a99bec0e
surafeljs <surafel.node@gmail.com>
2026-01-01 21:32:26 -0800

    update

--- cyber/src/App.jsx
+++ cyber/src/App.jsx
@@ -147,9 +147,11 @@ const logout=async()=>{
         to={'/logout'}
           onClick={logout}
           style={{
-    backgroundColor: "rgba(0, 0, 0, 0.4)", // REQUIRED for blur to be visible
-    marginBottom:'45px',
+              backgroundColor: "rgba(0, 0, 0, 0.4)", // REQUIRED for blur to be visible
+               marginBottom:'45px',
             width: "100%",
+              borderRadius:4,
+marginBottom:2,
             textAlign: "center",
             padding: "8px 0",
             cursor: "pointer",
